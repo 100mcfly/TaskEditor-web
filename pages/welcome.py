804a25bd09ae04e0ff7f736e4e05f92250358fcd
Odean <olorunmokehinde@gmail.com>
2025-04-01 18:26:48 +0100

    Implemented "italics" text format

--- pages/welcome.py
+++ pages/welcome.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.subheader("Welcome to my first web app!")
-st.write(" It might seem very simple but I'm really excited to share this with the world\n"
-         "Love, Dami")
+st.write(" It might seem very simple but I'm really excited to share this with the world")
+st.write("<i>Love, <b>Dami</b></i>", unsafe_allow_html=True)
